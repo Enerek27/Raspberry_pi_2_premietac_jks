9de7ad0f2551034da5893edf501d748676eff5fc
Enerek27 <tomhroncek@gmail.com>
2026-06-01 19:18:19 +0200

    asd

--- lib_premietac/premietac.c
+++ lib_premietac/premietac.c
@@ -49,29 +49,30 @@ static const char *get_current_sloha_text(StavPremietania *stav) {
 }
 
 void premietac_run_raylib(int uart_fd, const char *background_path) {
-  // ── raylib init na celý aktuálny monitor ─────────────────────────────
-  int monitor = GetCurrentMonitor();
-  int screenWidth = GetMonitorWidth(monitor);
-  int screenHeight = GetMonitorHeight(monitor); //[web:13][web:16]
-
-  InitWindow(screenWidth, screenHeight, "Premietac");
+  // 1) Inicializuj okno – 0,0 = celé plátno aktuálneho monitora (raylib >= 4.x)
+  InitWindow(0, 0, "Premietac");
   SetTargetFPS(60);
-  ToggleFullscreen(); //[web:10][web:16]
 
-  // Načítaj obrázok na pozadie (úvodný / medzi prezentáciami)
+  // Ak chceš mať istotu fullscreen režimu:
+  ToggleFullscreen();
+
+  int screenWidth = GetScreenWidth();
+  int screenHeight = GetScreenHeight();
+
+  // 2) Načítaj pozadie
   Texture2D background = LoadTexture(background_path);
   if (background.id == 0) {
-    printf("[Premietac] Nepodarilo sa načítať obrázok '%s'\n", background_path);
+    printf("[Premietac] Nepodarilo sa nacitat obrazok '%s'\n", background_path);
   }
 
   Rectangle bgSrc = {0};
   Rectangle bgDst = {0};
   if (background.id != 0) {
     compute_fullscreen_dest(background.width, background.height, screenWidth,
-                            screenHeight, &bgSrc, &bgDst); //[web:20]
+                            screenHeight, &bgSrc, &bgDst);
   }
 
-  // ── Stav protokolu / piesní ──────────────────────────────────────────
+  // 3) Stav protokolu
   StavPremietania stav;
   stav_init(&stav);
 
@@ -81,56 +82,34 @@ void premietac_run_raylib(int uart_fd, const char *background_path) {
   reasm_buf[0] = '\0';
   ParseovanyPrikaz prikaz;
 
-  // Font nastavenia
   int fontSize = 48;
-  int lineSpacing = 8;
 
   while (!WindowShouldClose()) {
-    // ── 1) UART: neblokujúce čítanie, spracovanie príkazov ───────────
     int len = uart_read_line_nonblock(uart_fd, line, sizeof(line));
-    if (len < 0) {
-      printf("[Premietac] Chyba čítania UART, končím\n");
+    if (len < 0)
       break;
-    }
     if (len > 0) {
-#ifdef DEBUG
-      printf("[RAW] %s\n", line);
-#endif
       if (reasm_pridaj_chunk(reasm_buf, &reasm_len, line, &prikaz)) {
-        // Máme komplet správu – zmení stav, vrátane
-        // SPUSTI/VYPNI/ZATMAV/ODTMAV/POSLI_PIESEN/...
         stav_aplikuj(&stav, &prikaz);
       }
     }
 
-    // ── 2) Kreslenie ────────────────────────────────────────────────
     BeginDrawing();
 
-    // Ak nebeží premietanie, zobraz úvodný obrázok
     if (!stav.bezi) {
       ClearBackground(BLACK);
       if (background.id != 0) {
         DrawTexturePro(background, bgSrc, bgDst, (Vector2){0, 0}, 0.0f, WHITE);
-      } else {
-        DrawText("CHYBA: background nenajdeny", 20, 20, 30, RED);
       }
     } else {
-      // Premietanie beží
       if (stav.blackscreen) {
         ClearBackground(BLACK);
       } else {
         ClearBackground(BLACK);
-
         const char *txt = get_current_sloha_text(&stav);
-
-        // jednoduché centrovanie textu horizontálne – MeasureText v
-        // pixeloch[web:15][web:18]
         int textWidth = MeasureText(txt, fontSize);
         int x = (screenWidth - textWidth) / 2;
         int y = screenHeight / 2 - fontSize / 2;
-
-        // Ak máš viacriadkový text, môžeš ho rozdeliť na riadky a kresliť po
-        // riadkoch.
         DrawText(txt, x, y, fontSize, WHITE);
       }
     }
@@ -138,7 +117,6 @@ void premietac_run_raylib(int uart_fd, const char *background_path) {
     EndDrawing();
   }
 
-  // Upratovanie
   if (background.id != 0)
     UnloadTexture(background);
   CloseWindow();
